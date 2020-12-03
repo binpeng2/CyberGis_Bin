65a82eaea8b0834d9c465084396cbcce8d6a6c5b
Han Wang <binpeng2@illinois.edu>
2020-12-02 23:26:34 -0500

    Up commands for my script

--- data_preprocess.py
+++ data_preprocess.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from collections import defaultdict
 import datetime
+import sys
 
 def createat2date(str):
     """Convert a datetime in string format to date
@@ -13,9 +14,10 @@ def createat2date(str):
         The date.
 
     """
+
     return str.split(' ')[0]
 
-def data2chunks(data_path, write_folder):
+def data2chunks(read_path, write_folder):
     """
 
     Read the raw twitter streaming dataset, delete unnecessary columns.
@@ -23,7 +25,7 @@ def data2chunks(data_path, write_folder):
     another, and finally write chunks to disk.
 
     Args:
-        data_path: the path of csv data
+        read_path: the path of csv data
         write_folder: the destination folder.
 
     Returns:
@@ -32,7 +34,7 @@ def data2chunks(data_path, write_folder):
     """
 
     chunksize = 10 ** 6
-    for i, chunk in enumerate(pd.read_csv(data_path, chunksize=chunksize)):
+    for i, chunk in enumerate(pd.read_csv(read_path, chunksize=chunksize)):
         print('Chunk: ', i)
         df = chunk.loc[:, ['currentGEOID', 'lon', 'lat', 'name', 'author_Id', 'created_at', 'location', 'place_type', 'place_full_name', 'place_country']]
         # print(df.head())
@@ -52,7 +54,7 @@ def data2chunks(data_path, write_folder):
         df.to_csv(write_folder + 'chunk_' + str(i) + '.csv', index=False)
         # break
 
-def chunks2dict(write_path, start, end):
+def chunks2dict(read_folder, write_path, start=0, end=330):
     """
 
     Read chunks from a fixed path. Then process index from start
@@ -60,6 +62,7 @@ def chunks2dict(write_path, start, end):
     map, which will finally be converted to a df and written down.
 
     Args:
+        read_folder: from this folder we read chunks
         write_path: where to write the final result
         start: the index of start chunk
         end: the index of end chunk
@@ -68,11 +71,12 @@ def chunks2dict(write_path, start, end):
         Nothing
 
     """
+
     m = defaultdict(lambda: defaultdict(int))
     last_id = ''
     for i in range(start, end):
         print("Processing: ", i)
-        df = pd.read_csv("./chunks_2/chunk_"+str(i)+".csv")
+        df = pd.read_csv(read_folder + "/chunk_"+str(i)+".csv")
         for index, row in df.iterrows():
             if row['author_Id'] == last_id:
                 place = row['currentGEOID']
@@ -101,7 +105,7 @@ def chunks2dict(write_path, start, end):
         df.at[index, "ISO3"] = geoInfo["ISO3"].values[0] if len(geoInfo["ISO3"].values) > 0 else "NaN"
     df.to_csv(write_path)
 
-def accu_dict(time_length):
+def accu_dict(read_path, write_path, time_length):
     """
 
     This function accumulate any days visitors in each cell. For example,
@@ -109,6 +113,8 @@ def accu_dict(time_length):
     will be 28.
 
     Args:
+        read_path: the path from which we read our data
+        write_path: the path we write our data to (notice, we will add time_length as suffix)
         time_length: The time length we want to accumulate
 
     Returns:
@@ -117,7 +123,8 @@ def accu_dict(time_length):
         days of visitors.
 
     """
-    df = pd.read_csv("./dicts_2/dict_0-329.csv")
+
+    df = pd.read_csv(read_path)
     for i in range(0, 1149):
         visitors_num = []
         for j in range(7, 126):
@@ -127,7 +134,7 @@ def accu_dict(time_length):
             time_accu = sum(visitors_num)
             # print((df.iloc[:, j].at[i], seven_accu, fourteen_accu))
             df.iloc[:, j].at[i] = time_accu
-    df.to_csv("./dicts_2/accu_dict_0-329_" + str(time_length) + ".csv")
+    df.to_csv(write_path + str(time_length) + ".csv")
 
 def calc_percent(read_path, write_path):
     """
@@ -146,6 +153,7 @@ def calc_percent(read_path, write_path):
         number of past time_length days of visitors.
 
     """
+
     df = pd.read_csv(read_path)
     for i in range(0, 1149):
         prev_num = 0
@@ -166,14 +174,35 @@ def calc_percent(read_path, write_path):
     df.to_csv(write_path)
 
 if __name__ == "__main__":
-    # chunksize = 10 ** 6
     # data2chunks("./twitter.streaming6_byHuman_20200214_20200612/twitter.streaming6_byHuman_20200214_20200612.csv", './chunks_2/')
     # chunks2dict("./dicts_2/dict_0-329"+".csv", 0, 330)
     # accu_dict(14)
-    calc_percent("./dicts_2/accu_dict_0-329_7"+".csv", "./dicts_2/accu_dict_0-329_7_percent"+".csv")
-    # df = pd.read_csv("./dicts_2/accu_dict_0-329.csv")
-    # print(df.head)
-    # print(df.iloc[1]['2/14/2020'])
-    # res = tuple(map(int, df.iloc[1]['2/14/2020'][1:-1].split(', ')))
-    # print(res)
-    # print(res[0])
+    # calc_percent("./dicts_2/accu_dict_0-329_7"+".csv", "./dicts_2/accu_dict_0-329_7_percent"+".csv")
+    function_name = str(sys.argv[1])
+    if function_name == "all":
+        # To use this command, please make sure that you have already created the two folders
+        # ./chunks_folder and ./dicts_folder
+        data2chunks("./twitter.streaming6_byHuman_20200214_20200612/twitter.streaming6_byHuman_20200214_20200612.csv", './chunks_folder/')
+        chunks2dict("./chunks_folder/", "./dicts_folder/dict_0-329.csv")
+        accu_dict(14)
+        calc_percent("./dicts_folder/accu_dict_0-329_14.csv", "./dicts_folder/accu_dict_0-329_14_percent.csv")
+    elif function_name == "data2chunks":
+        read_path = str(sys.argv[2])
+        write_folder = str(sys.argv[3])
+        data2chunks(read_path, write_folder)
+    elif function_name == "chunks2dict":
+        read_folder = str(sys.argv[2])
+        write_path = str(sys.argv[3])
+        chunks2dict(read_folder, write_path)
+    elif function_name == "accu_dict":
+        read_path = str(sys.argv[2])
+        write_path = str(sys.argv[3])
+        time_length = int(sys.argv[4])
+        accu_dict(read_path, write_path, time_length)
+    elif function_name == "calc_percent":
+        read_path = str(sys.argv[2])
+        write_path = str(sys.argv[3])
+        calc_percent(read_path, write_path)
+    else:
+        print("Please enter correct function name")
+        exit(1)
